020573e6999d36134bf70c2f0efd1efd311f51d2
brunathaisjc <btgnasc@gmail.com>
2025-08-11 14:25:57 -0300

    classificacao simples usando condicionais

--- conceitos-basicos-ia.py
+++ conceitos-basicos-ia.py
@@ -35,6 +35,37 @@ alvo_nao_existente = 100
 indice_existente = busca_linear(numeros, alvo_existente)
 indice_nao_existente = busca_linear(numeros, alvo_nao_existente)
 
-print(f'Lista de números: {numeros}')
+print(f'\nLista de números: {numeros}')
 print(f'Buscando por {alvo_existente}: Índice {indice_existente}')
 print(f'Buscando por {alvo_nao_existente}: Índice {indice_nao_existente}')
+
+# Regras simples em Python
+
+def classificar_num(numero):
+    """
+    Classificar em 'pequeno', 'médio' ou 'grande'.
+    Argumentos:
+
+    Retornos: 
+
+
+    """
+    if numero < 10:
+        return "pequeno"
+    elif numero <= 10:
+        return "medio"
+    elif numero <= 50:
+        return "medio"
+    else:
+        return "grande"
+    
+num1 = 13
+num2 = 2
+num3 = 60
+num4 = 50
+num4 = 4
+
+print(f"\nClassificando {num1}: {classificar_num(num1)}")
+print(f"Classificando {num2}: {classificar_num(num2)}")
+print(f"Classificando {num3}: {classificar_num(num3)}")
+print(f"Classificando {num4}: {classificar_num(num4)}\n")
